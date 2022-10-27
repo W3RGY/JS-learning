09708603f3486968a82c848908b381ac9a639487
Dima Osenkov <w3rgy1992@gmail.com>
2022-10-27 21:39:29 +0200

    first commit

---
+++ script1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const result = confirm("Are you here ?")      //modal window
+// console.log(result);
+
+// const answer = +prompt("are you 19?", "");        //question modal window
+// console.log(answer + 5);					 //typeof() - data type
+
+const answers = [];		//putting data into an massif
+
+answers[0] = prompt('Your name?', '');
+answers[1] = prompt('Your surname?', '');
+answers[2] = prompt('How old are you ?', '');
+
+document.write(answers);
---
+++ script2.js
@@ -0,0 +1,9 @@
+"use strict";		// interpolation
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Den";
+
+alert(`Hello, ${user}`);
