4a492a8ddcdf254883a40d107950f8160e94f2a1
Dima Osenkov <w3rgy1992@gmail.com>
2022-11-02 18:19:11 +0100

    Loops

--- script6.js
+++ script6.js
@@ -2,7 +2,21 @@
 
 let num = 50;
 
-while (num < 55) {
-	console.log(num);
-	num++;
+// while (num <= 55) {		// Loops(циклы)
+// 	console.log(num);
+// 	num++;
+// }
+
+// do {						// Loops
+// 	console.log(num);
+// 	num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {		// Loops
+	if (i === 6) {
+		// break;		// breaks the loop at this point i===6
+		continue;		// skip that step i===6, and continue
+	}
+	console.log(i);
 }
