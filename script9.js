86dc5d8aa7aae9ad34129b89c35f032facad7ff0
Dima Osenkov <w3rgy1992@gmail.com>
2022-11-16 21:01:23 +0100

    function

--- script9.js
+++ script9.js
@@ -82,39 +82,37 @@ console.log(doNothing() === undefined);
 //======================================
 
 function sayHello(name) {
-    return `Привет, ${name}!`;
+	return `Привет, ${name}!`;
 }
+console.log(sayHello('Alex'));
 
-sayHello('Alex');
+
 
 function returnNeighboringNumbers(num) {
     return [num - 1, num, num + 1];
 }
+console.log(returnNeighboringNumbers(5));
 
-returnNeighboringNumbers(5);
+
 
 function getMathResult(num, times) {
     if (typeof(times) !== 'number' || times <= 0) {
         return num;
     }
-
     let str = '';
-
     for (let i = 1; i <= times; i++) {
         if (i === times) {
             str += `${num * i}`;
-            // Тут без черточек в конце
         } else {
             str += `${num * i}---`;
             // Это тоже самое, что и
             // str = str + num * i + "---"
         }
     }
-
     return str;
 }
+console.log(getMathResult(10, 5));
 
-getMathResult(10, 5);
 
 
 
