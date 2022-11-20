e5329118c8d499a29d70983058d5dd6fbcdac768
Dima Osenkov <w3rgy1992@gmail.com>
2022-11-20 20:02:53 +0100

    forwhile(function)

--- script6.js
+++ script6.js
@@ -39,6 +39,24 @@ arr.forEach(function (item, i, array) {
 	Массив: ${array}`);
 });
 
+
+const lines = 5;
+let result = '';
+// Проверяется именно переменная result, формируйте строку в ней
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result);
+
+
 function fifthTask() {
     const arrayOfNumbers = [];
     for (let i = 5; i < 11; i++) {
