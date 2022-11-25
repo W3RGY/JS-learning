90b589c17ea23871a192660195874f793c6708b6
Dima Osenkov <w3rgy1992@gmail.com>
2022-11-25 20:31:27 +0100

    function

--- script9.js
+++ script9.js
@@ -211,6 +211,37 @@ findMaxNumber(1, 5, 6.6, 10.5);
 findMaxNumber(1, 5, '6', '10');
 
 
+
+function fib(num) {
+    if (typeof(num) !== 'number' || num <= 0 || !Number.isInteger(num)) {
+        return "";
+    }
+
+    let result = '';
+    let first = 0;
+    let second = 1;
+
+    for (let i = 0; i < num; i++) {
+        if (i + 1 === num) {
+            result += `${first}`;
+            // Без пробела в конце
+        } else {
+            result += `${first} `;
+        }
+
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+
+    return result;
+}
+
+fib(5)
+
+
+
+
 //========================================================================================================================================================
 //Объявление функции (Function Declaration)
 /*
