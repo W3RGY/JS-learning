5cc446272ccf0b74f6aa13c244fe9d835bbb6e35
Dima Osenkov <w3rgy1992@gmail.com>
2022-12-07 17:11:34 +0100

    function

--- script9.js
+++ script9.js
@@ -144,7 +144,6 @@ function getCoupeNumber(seatNumber) {
     if (typeof (seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
         return "Ошибка. Проверьте правильность введенного номера места";
     }
-
     if (seatNumber === 0 || seatNumber > 36) {
         return "Таких мест в вагоне не существует";
     }
